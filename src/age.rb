b6f3725c742fdbf34c63c1f82b698395987232c8
Pablo Romanelli <pabloromanelli@gmail.com>
2015-04-11 01:29:34 -0300

    Codigo antes de clase 2

--- src/age.rb
+++ src/age.rb
@@ -1,10 +1,34 @@
+module Observable
+  attr_accessor :interesados
+
+  def interesados
+    @interesados = @interesados || []
+    @interesados
+  end
+
+  def nuevo_interesado(interesado)
+    self.interesados << interesado
+  end
+
+  def notificar
+    self.interesados.each { |interesado|
+      interesado.call(self)
+    }
+  end
+end
+
 module Defensor
+  include Observable
   attr_accessor :energia, :potencial_defensivo
 
   def sufri_danio(danio)
     self.energia= self.energia - danio
+    self.notificar
   end
 
+  def descansar
+    @energia += 10
+  end
 end
 
 module Atacante
@@ -15,18 +39,34 @@ module Atacante
       danio = self.potencial_ofensivo - un_defensor.potencial_defensivo
       un_defensor.sufri_danio(danio)
     end
+    @descansado = false
   end
 
+  def potencial_ofensivo
+    # nil en ruby evalua es false
+    @descansado ? @potencial_ofensivo * 2 : @potencial_ofensivo
+  end
+
+  def descansar
+    @descansado = true
+  end
 end
 
 class Guerrero
   include Defensor
+  alias_method :descansar_defensor, :descansar
   include Atacante
+  alias_method :descansar_atacante, :descansar
 
-  def initialize(potencial_ofensivo=20, energia=100, potencial_defensivo=10)
+  def initialize(potencial_ofensivo = 20, energia = 100, potencial_defensivo = 10)
     self.potencial_ofensivo = potencial_ofensivo
     self.energia = energia
     self.potencial_defensivo = potencial_defensivo
+  end
+
+  def descansar
+    descansar_atacante
+    descansar_defensor
   end
 end
 
@@ -40,9 +80,8 @@ class Espadachin < Guerrero
   end
 
   def potencial_ofensivo
-    super() + self.espada.potencial_ofensivo
+    super + self.espada.potencial_ofensivo
   end
-
 end
 
 class Espada
@@ -63,8 +102,74 @@ end
 
 class Muralla
   include Defensor
+
   def initialize
-    self.energia =200
+    self.energia = 200
     self.potencial_defensivo = 50
   end
+end
+
+class Kamikaze
+  include Defensor
+  include Atacante
+
+  def initialize
+    self.energia = 100
+    self.potencial_ofensivo = 250
+    self.potencial_defensivo = 30
+  end
+
+  def atacar(un_defensor)
+    super
+    self.energia = 0
+  end
+end
+
+class Mago
+  attr_accessor :teletransportando
+
+  def initialize
+    @teletransportando = []
+  end
+
+  def curar(unidad)
+    unidad.energia += 20
+  end
+
+  def teletransportar(unidad)
+    @teletransportando << unidad
+  end
+
+  def lastimaron_a(unidad)
+    self.curar(unidad)
+  end
+end
+
+class Ejercito
+  attr_accessor :retirandose, :estrategia
+
+  def self.cobarde
+    ejercito = Ejercito.new
+    ejercito.estrategia = proc { |defensor|
+      ejercito.retirandose = true
+    }
+    ejercito
+  end
+
+  def self.descansador
+    ejercito = Ejercito.new
+    ejercito.estrategia = proc { |defensor|
+      defensor.descansar
+    }
+    ejercito
+  end
+
+  def initialize
+    self.retirandose = false
+  end
+
+  def add(defensor)
+    defensor.nuevo_interesado(self.estrategia)
+  end
+
 end
